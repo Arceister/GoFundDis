568e68f546ccc639937f89f8f574bb586510e226
arceister <imandajagad01@gmail.com>
2022-06-27 22:44:40 +0700

    Test(User): add `invalid without email` test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe User, type: :model do
   it "is valid with email, password, name, and phone" do
     expect(FactoryBot.build(:user)).to be_valid
   end
+
+  it "is invalid without an email" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 end
