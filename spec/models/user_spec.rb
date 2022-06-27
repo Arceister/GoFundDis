eb1a7ca70562588c985dfd9df111d03cb82ac533
arceister <imandajagad01@gmail.com>
2022-06-27 22:19:49 +0700

    Test(User): add `valid email, password, name, and phone` test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(FactoryBot.build(:user)).to be_valid
   end
+
+  it "is valid with email, password, name, and phone" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 end
