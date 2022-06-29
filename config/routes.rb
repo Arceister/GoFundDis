422088381a5a4e802251afd450d14bd4f73bf4a2
arceister <imandajagad01@gmail.com>
2022-06-29 21:09:11 +0700

    Add(View): add root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "main#index"
+
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
 
