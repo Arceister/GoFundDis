8b1495c89ff0196db350cc3b6ab9a22bad8a4f97
arceister <imandajagad01@gmail.com>
2022-06-29 16:43:23 +0700

    Add(View): add `new` view for user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "login", to: "sessions#new"
-  post "login" to: "sessions#create"
+  post "login", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
 end
