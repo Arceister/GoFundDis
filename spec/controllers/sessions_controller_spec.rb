51c221b576b40e7542cdebb37d3985f51fcd18fc
arceister <imandajagad01@gmail.com>
2022-06-29 15:07:31 +0700

    Test(User Authentication): add new behavior on `Destroy`

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SessionsController do
   describe 'Destroy' do
-    
+    before :each do
+      @user = create(:user)
+      session[:user_id] = @user.id
+    end
   end
 end
