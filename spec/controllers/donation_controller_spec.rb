ebdb528d2accdd81fc7af8b715c027142256fc09
arceister <imandajagad01@gmail.com>
2022-06-30 14:29:36 +0700

    Test(Donation Controller): add `renders #show` testcase

--- spec/controllers/donation_controller_spec.rb
+++ spec/controllers/donation_controller_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe DonationsController do
   describe 'GET #show' do
     it "returns a 200 HTTP Status" do
       @donation = create(:donation)
-      get :show, params: {id: @donation.id}
+      get :show, params: {id: @donation}
       expect(response).to have_http_status(:ok)
+    end
+
+    it "renders #show template" do
+      @donation = create(:donation)
+      get :show, params: {id: @donation}
+      expect(response).to render_template(:show)
     end
   end
 end
