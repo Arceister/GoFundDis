07a95937c8897ba86e3fa65d8d0164872afabf48
arceister <imandajagad01@gmail.com>
2022-06-30 14:27:36 +0700

    Test(Donation Controller): add `returns 200 HTTP status` testcase

--- spec/controllers/donation_controller_spec.rb
+++ spec/controllers/donation_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DonationsController do
-  
+  describe 'GET #show' do
+    it "returns a 200 HTTP Status" do
+      @donation = create(:donation)
+      get :show, params: {id: @donation.id}
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
