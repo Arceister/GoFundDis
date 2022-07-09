5f2b80132986dc03e9e25d2b1c8db670d02f2f9b
arceister <imandajagad01@gmail.com>
2022-07-10 02:02:34 +0700

    Test(Donation Details): add `invalid without metode` testcase

--- app/models/donation_detail.rb
+++ app/models/donation_detail.rb
@@ -1,4 +1,6 @@
 class DonationDetail < ApplicationRecord
   belongs_to :user
   belongs_to :donation
+
+  validates :metode, presence: true
 end
--- spec/models/donation_detail_spec.rb
+++ spec/models/donation_detail_spec.rb
@@ -29,4 +29,17 @@ RSpec.describe DonationDetail, type: :model do
 
     expect(@donation_detail.errors[:donation]).to include("must exist")
   end
+
+  it "is invalid without metode" do
+    @user = create(:user)
+    @donation = create(:donation, user_id: @user.id)
+
+    @donation_detail = build(:donation_detail, 
+      user_id: @user.id,
+      donation_id: @donation.id,
+      metode: nil)
+    @donation_detail.valid?
+
+    expect(@donation_detail.errors[:metode]).to include("can't be blank")
+  end
 end
