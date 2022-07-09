a8b77c40a03a32179cf63ea5554aaa75d901fb8d
arceister <imandajagad01@gmail.com>
2022-07-10 01:46:11 +0700

    Test(Donation Details): add `invalid without user id` testcase

--- spec/models/donation_detail_spec.rb
+++ spec/models/donation_detail_spec.rb
@@ -3,10 +3,20 @@ require 'rails_helper'
 RSpec.describe DonationDetail, type: :model do
   it "has a valid factory" do
     @user = create(:user)
-    @donation = create(:donation)
-    
+    @donation = create(:donation, user_id: @user.id)
+
     expect(build(:donation_detail, 
       user_id: @user.id, 
       donation_id: @donation.id)).to be_valid
+  end
+
+  it "is invalid without user id" do
+    @user = create(:user)
+    @donation = create(:donation, user_id: @user.id)
+
+    @donation_detail = build(:donation_detail, donation_id: @donation.id)
+    @donation_detail.valid?
+
+    expect(@donation_detail.errors[:user]).to include("must exist")
   end
 end
